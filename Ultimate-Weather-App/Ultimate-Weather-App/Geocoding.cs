09fa16de6c815bd0c6e201ae75af62cb4a0f5d82
Jonathan Raya Rios <jonathan17699@hotmail.es>
2022-07-28 09:36:32 +0200

    Added ID for the geocoding results

--- Ultimate-Weather-App/Ultimate-Weather-App/Geocoding.cs
+++ Ultimate-Weather-App/Ultimate-Weather-App/Geocoding.cs
@@ -2,8 +2,14 @@
 {
     public class Geocoding
     {
+        public Guid Id { get; set; }
         public string Latitude { get; set; }
         public string Longitude { get; set; }
         public string Label { get; set; }
+
+        public Geocoding()
+        {
+            Id = Guid.NewGuid();
+        }
     }
 }
